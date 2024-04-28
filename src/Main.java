57a825a8871578e0a80476a4eb1cae09fbe93659
chueshov.dmitrii <chueshov.dmitrii@otr.ru>
2024-04-28 20:18:38 +0300

    added in main sout is2

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("feature is1");
+        System.out.println("feature is2");
+        System.out.println("feature is2");
+
     }
 }
