ff9f833441cd58256bbc020ed7eea04c056363ae
chueshov.dmitrii <chueshov.dmitrii@otr.ru>
2024-04-28 20:46:58 +0300

    added in main sout is2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println("feature is1");
         System.out.println("feature is2");
         System.out.println("feature is2");
+        System.out.println("feature is2");
 
+        System.out.println("feature is2");
     }
 }
